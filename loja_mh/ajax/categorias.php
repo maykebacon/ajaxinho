f60dbe465e9041fb1f83bfdf08818f08a30aa4f6
Mayke brayan <maykebrayan88@gmail.com>
2018-06-20 14:26:02 -0300

    Add files via upload

---
+++ loja_mh/ajax/categorias.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+</head>
+    <body>
+
+
+    <ul>
+
+<?php
+    require_once "../app/models/CategoriaCrud.php";
+
+    $crud = new CategoriaCrud();
+
+    $categorias = $crud->getCategorias();
+
+    foreach ($categorias as $categoria){
+        echo "<li>".utf8_encode($categoria->getNome())."</li>";
+    }
+
+?>
+
+
+
+    </ul>
+
+
+</body>
+
+
+
+</html>
